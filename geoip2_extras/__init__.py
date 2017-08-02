a0ad14b39f8b28c998eac0a44e3dbab9a87e7380
Hugo Rodger-Brown <hugo@yunojuno.com>
2017-08-02 11:13:21 +0100

    Add app.py and AppConfig

---
+++ geoip2_extras/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'geoip2_extras.apps.GeoIP2ExtrasAppConfig'
---
+++ geoip2_extras/apps.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from django.apps import AppConfig
+
+
+class GeoIP2ExtrasAppConfig(AppConfig):
+
+    name = 'geoip2_extras'
+    verbose_name = "GeoIP2 Extras"
+    configs = []
